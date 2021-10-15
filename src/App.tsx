82f2074ca6cfacfb57a03e419f4acd3bc42cfb8c
Abin Joseph <itsmeabjs@gmail.com>
2021-10-15 17:37:41 +0530

    feat: deployment setup done and data feching also done

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,25 @@
+import { useEffect, useState } from "react";
 import { AiOutlineSetting } from "react-icons/ai";
 export default function App() {
+  const [reviewData, setReviewData] = useState<Props[] | []>([]);
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch(
+        "https://raw.githubusercontent.com/abjs/kerala-blood-bank/main/database.json"
+      );
+      try {
+        const { data } = await response.json();
+        setReviewData(data);
+      } catch (error: any) {
+        console.log(error);
+        if (error.message === "Unexpected token < in JSON at position 0") {
+          alert("Please check your internet connection and try again");
+        }
+      }
+    }
+    fetchData();
+  }, []);
+  console.log(reviewData);
   return (
     <div className="h-screen flex items-center justify-center flex-col">
       <AiOutlineSetting className="w-24 h-24 animate-spin-slow pb-5" />
@@ -10,4 +30,14 @@ export default function App() {
       </div>
     </div>
   );
+}
+
+interface Props {
+  name: string;
+  phone: string;
+  blood_group: "O+" | "O-" | "A+" | "A-" | "B+" | "B-" | "AB+" | "AB-";
+  address: string;
+  district: string;
+  state: string;
+  country: string;
 }
